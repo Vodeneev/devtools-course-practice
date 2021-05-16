2f2d1a047d057c4c52bbabc1746fecf2bece656f
Vodeneev <55285701+Vodeneev@users.noreply.github.com>
2021-05-16 22:31:22 +0300

    Small fix4

--- modules/modified_stack_minimums/include/modify_stack_app.h
+++ modules/modified_stack_minimums/include/modify_stack_app.h
@@ -9,13 +9,13 @@
 class StackApp {
  public:
 StackApp();
-	std::string operator()(int argc, const char** argv);
+    std::string operator()(int argc, const char** argv);
 
  private:
-	void help(const char* appname, const char* message = "");
-	bool validateNumberOfArguments(int argc, const char** argv);
-	std::string message_;
-	ModifiedStack stack;
+    void help(const char* appname, const char* message = "");
+    bool validateNumberOfArguments(int argc, const char** argv);
+    std::string message_;
+    ModifiedStack stack;
 };
 
 #endif  // MODULES_MODIFIED_STACK_MINIMUMS_INCLUDE_MODIFY_STACK_APP_H_
